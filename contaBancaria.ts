3c934312ee434d549e3de5348ebf99226c89adec
gabigpalharini <gabigplharini@gmail.com>
2023-08-24 13:54:46 -0300

    atualizando conta bancaria

--- contaBancaria.ts
+++ contaBancaria.ts
@@ -32,7 +32,22 @@ return msgSacarsaldo + saque + " O saldo atual é  " +this.saldoatual;
 }
 let minhaconta = new ContaBancaria("João", 1000);
 console.log(minhaconta.consultarSaldotitular())
+console.log(minhaconta.consultarSaldotitular())
 console.log(minhaconta.depositar(500));
 console.log(minhaconta.sacar(200));
 console.log(minhaconta.sacar(1500));
+
+let minhaconta2 = new ContaBancaria("gabi", 6000);
+console.log(minhaconta.consultarSaldotitular())
+console.log(minhaconta.consultarSaldotitular())
+console.log(minhaconta.depositar(500));
+console.log(minhaconta.sacar(300));
+console.log(minhaconta.sacar(7000));
+
+let minhaconta3 = new ContaBancaria("emilly", 2500);
+console.log(minhaconta3.consultarSaldotitular())
+console.log(minhaconta3.consultarSaldotitular())
+console.log(minhaconta3.depositar(1000));
+console.log(minhaconta3.sacar(150));
+console.log(minhaconta3.sacar(4000));
 

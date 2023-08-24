c55df3e77c9d038c48bd6182212048c9e44d74bd
gabigpalharini <gabigplharini@gmail.comconsole.log(minhaconta.consultarSaldotitular())>
2023-08-24 14:58:03 -0300

    atualizando contaBancaria

--- contaBancaria.ts
+++ contaBancaria.ts
@@ -31,23 +31,20 @@ return msgSacarsaldo + saque + " O saldo atual é  " +this.saldoatual;
 } 
 }
 let minhaconta = new ContaBancaria("João", 1000);
-console.log(minhaconta.consultarSaldotitular())
-console.log(minhaconta.consultarSaldotitular())
+console.log(minhaconta.consultarSaldotitular());
 console.log(minhaconta.depositar(500));
 console.log(minhaconta.sacar(200));
 console.log(minhaconta.sacar(1500));
 
-let minhaconta2 = new ContaBancaria("gabi", 6000);
-console.log(minhaconta.consultarSaldotitular())
-console.log(minhaconta.consultarSaldotitular())
-console.log(minhaconta.depositar(500));
-console.log(minhaconta.sacar(300));
-console.log(minhaconta.sacar(7000));
+let minhacontadois = new ContaBancaria("gabi", 6000);
+console.log(minhacontadois.consultarSaldotitular())
+console.log(minhacontadois.depositar(500));
+console.log(minhacontadois.sacar(300));
+console.log(minhacontadois.sacar(7000));
 
-let minhaconta3 = new ContaBancaria("emilly", 2500);
-console.log(minhaconta3.consultarSaldotitular())
-console.log(minhaconta3.consultarSaldotitular())
-console.log(minhaconta3.depositar(1000));
-console.log(minhaconta3.sacar(150));
-console.log(minhaconta3.sacar(4000));
+let minhacontatres = new ContaBancaria("emilly", 2500);
+console.log(minhacontatres.consultarSaldotitular());
+console.log(minhacontatres.depositar(1000));
+console.log(minhacontatres.sacar(150));
+console.log(minhacontatres.sacar(4000));
 

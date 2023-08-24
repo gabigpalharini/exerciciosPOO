38b8c1ecb9c0bbe17fcab9ed47ac6a024ee87af0
gabigpalharini <gabigplharini@gmail.comconsole.log(minhaconta.consultarSaldotitular())>
2023-08-24 15:47:05 -0300

    atualizando estudante

---
+++ estudante.ts
@@ -0,0 +1,35 @@
+class Estudante {
+    nomeestudante: string;
+    idadeestudante: number;
+    notas: number[];
+
+
+    constructor(nomeestudante: string, idadeestudante: number, notas: number[]) {
+        this.nomeestudante = nomeestudante;
+        this.idadeestudante = idadeestudante;
+        this.notas = notas;
+
+    }
+
+
+    media() {
+        let totalnotas = 0;
+        let media = 0;
+        for (let i = 0; i < this.notas.length; i++) {
+            totalnotas = totalnotas + this.notas[i];
+        }
+        media = totalnotas / this.notas.length;
+        let msgmedia = "Parabéns " + this.nomeestudante + " sua média é " + media + " e você está aprovado";
+        if (media < 7) {
+         msgmedia = " sua média  é " + media + "e você precisa estudar mais para melhorar seu desempenho";
+       
+
+        }
+        return msgmedia;
+    }
+
+
+}
+let minhamedia = new Estudante("Maria,", 20, [8, 7.5, 9.2, 6.8, 9.5]);
+console.log(minhamedia.media());
+
